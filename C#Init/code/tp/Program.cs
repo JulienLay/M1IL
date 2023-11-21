5a0bef55094974b3ac15e4feca0e4794116fc10f
Julien Lay <58702706+JulienLay@users.noreply.github.com>
2023-11-21 14:49:52 +0100

    Add files via upload

--- C#Init/code/tp/Program.cs
+++ C#Init/code/tp/Program.cs
@@ -6,25 +6,41 @@ namespace Initiation
     {
         public static void Main(string[] args)
         {
-        //  exo_1();
-        //  exo_2();
-        //  exo_3();
-        //  exo_4_1();
-        //  exo_4_2();
-        //  exo_4_3();
-        // exo_6("ROUGE");
-        // exo_7();
-        // exo_9();
-        // exo_10();
-        // exo_11();
-        // Console.WriteLine(exo_12(7));
-        // exo_13();
-        // exo_14();
-        // exo_15();
-        // exo_16();
-        // exo_17();
-        // exo_18();
-            exo_19();
+            //  exo_1();
+            //  exo_2();
+            //  exo_3();
+            //  exo_4_1();
+            //  exo_4_2();
+            //  exo_4_3();
+            // exo_6("ROUGE");
+            // exo_7();
+            // exo_9();
+            // exo_10();
+            // exo_11();
+            // Console.WriteLine(exo_12(7));
+            // exo_13();
+            // exo_14();
+            // exo_15();
+            // exo_16();
+            // exo_17();
+            // exo_18();
+            // exo_19();
+            // POUR L'EXO 20
+            // int[] array1 = { 0, 1, 2, 3 };
+            // int[] array2 = { 6, 7, 8 };
+            // int index = 2;
+
+            // int[] array3 = exo_20(array1, array2, index);
+
+            // Console.WriteLine("array3:");
+            // foreach (int number in array3)
+            // {
+            //     Console.Write(number + " ");
+            // }
+            // Console.WriteLine();
+            // exo_21();
+            // exo_22();
+            exo_23();
         }
 
         public static void exo_1()
@@ -282,7 +298,7 @@ namespace Initiation
             // Remplir le tableau avec les chiffres entre 5 et 13
             for (int i = 0; i < tableau.Length; i++)
             {
-                tableau[i] = 5 + i;
+                tableau[i] = i + 5;
             }
 
             // Remplacer le 3e élément du tableau par 111
@@ -322,48 +338,155 @@ namespace Initiation
             Console.WriteLine();
         }
 
-        // static void exo_19() {
-        //     int[] tableau1 = { 1, 3, 5, 7 };
-        //     int[] tableau2 = { 2, 4, 6, 8, 10 };
+        static void exo_19() {
+            int[] tableau1 = { 1, 3, 5, 7 };
+            int[] tableau2 = { 2, 4, 6, 8, 10, 12, 45 };
+            int i = 0;
+            int j = 0;
+            int k = 0;
+            int[] tableauResultat = new int[tableau1.Length + tableau2.Length];
 
-        //     int[] nouveauTableau = FusionnerTableaux(tableau1, tableau2);
+            if (tableau1.Length < tableau2.Length) {
+                while (i<tableau1.Length) {
+                    tableauResultat[k] = tableau1[i];
+                    tableauResultat[k+1] = tableau2[j];
+                    i++;
+                    j++;
+                    k+=2;
+                }
+                while (j<tableau2.Length) {
+                    tableauResultat[k] = tableau2[j];
+                    j++;
+                    k++;
+                }
+            } else if (tableau1.Length > tableau2.Length) {
+                while (j<tableau2.Length) {
+                    tableauResultat[k] = tableau1[i];
+                    tableauResultat[k+1] = tableau2[j];
+                    i++;
+                    j++;
+                    k+=2;
+                }
+                while (i<tableau1.Length) {
+                    tableauResultat[k] = tableau1[i];
+                    i++;
+                    k++;
+                }
+            } else {
+                while (i<tableau1.Length) {
+                    tableauResultat[k] = tableau1[i];
+                    tableauResultat[k+1] = tableau2[j];
+                    i++;
+                    j++;
+                    k+=2;
+                }
+            }
 
-        //     Console.WriteLine("Nouveau tableau fusionné :");
-        //     AfficherTableau(nouveauTableau);
-        // }
+            Console.WriteLine("\nTableau final :");
+            foreach (int nombre in tableauResultat)
+            {
+                Console.Write(nombre + " ");
+            }
+            Console.WriteLine();
+        }
 
-        // static int[] FusionnerTableaux(int[] tableau1, int[] tableau2)
-        // {
-        //     int longueurNouveauTableau = tableau1.Length + tableau2.Length;
-        //     int[] nouveauTableau = new int[longueurNouveauTableau];
+        // Écrire une fonction qui insère un tableau à un index précis dans un autre tableau.
+        // Exemple
+        // array1 = {0,1,2,3};
+        // array2 = {6,7,8};
+        // array3 = F(array1,array2,2);
+        //array3
+        // {0,1,6,7,8, 2,3}
+        static int[] exo_20(int[] destinationArray, int[] sourceArray, int insertionIndex) {
+            int totalLength = destinationArray.Length + sourceArray.Length;
+            int[] resultArray = new int[totalLength];
 
-        //     int indexTableau1 = 0;
-        //     int indexTableau2 = 0;
+            // Copier les éléments de destinationArray avant l'index spécifié
+            for (int i = 0; i < insertionIndex; i++)
+            {
+                resultArray[i] = destinationArray[i];
+            }
 
-        //     for (int i = 0; i < longueurNouveauTableau; i++)
-        //     {
-        //         if (indexTableau1 < tableau1.Length)
-        //         {
-        //             nouveauTableau[i] = tableau1[indexTableau1];
-        //             indexTableau1++;
-        //         }
-        //         else if (indexTableau2 < tableau2.Length)
-        //         {
-        //             nouveauTableau[i] = tableau2[indexTableau2];
-        //             indexTableau2++;
-        //         }
-        //     }
+            // Copier les éléments de sourceArray à partir de l'index spécifié
+            for (int i = 0; i < sourceArray.Length; i++)
+            {
+                resultArray[insertionIndex + i] = sourceArray[i];
+            }
 
-        //     return nouveauTableau;
-        // }
+            // Copier les éléments restants de destinationArray après l'insertion de sourceArray
+            for (int i = insertionIndex; i < destinationArray.Length; i++)
+            {
+                resultArray[sourceArray.Length + i] = destinationArray[i];
+            }
 
-        // static void AfficherTableau(int[] tableau)
-        // {
-        //     foreach (int nombre in tableau)
-        //     {
-        //         Console.Write(nombre + " ");
-        //     }
-        //     Console.WriteLine();
-        // }
+            return resultArray;
+        }
+
+        // Ecrire un programme qui remplit
+        // un tableau avec tous les caractères
+        // d’un mot tapé par l’utilisateur.
+        // •
+        // Afficher le mot contenu dans le
+        // tableau en intercalent un « »
+        // entre chaque lettre.
+        // •
+        // Inverser le contenu du tableau puis
+        // afficher le mot obtenu dans la
+        // console.
+        public static void exo_21()
+        {
+            Console.WriteLine("Veuillez taper un mot : ");
+            string? mot = Console.ReadLine();
+            char[] motCharArray = mot.ToCharArray();
+            for (int i=0; i < motCharArray.Length; i++) {
+                Console.WriteLine(motCharArray[i]);
+                if (i != motCharArray.Length - 1) {
+                    Console.WriteLine(".");
+                }
+            }
+
+            int j = motCharArray.Length - 1;
+            for(int i=0; i<motCharArray.Length/2; i++) {
+                char temp = motCharArray[i];
+                motCharArray[i] = motCharArray[j];
+                motCharArray[j] = temp;
+                j--;
+            }
+
+            Console.Write("\nMot inversé :");
+            for (int i = 0; i < motCharArray.Length; i++)
+            {
+                Console.Write(motCharArray[i] + " ");
+            }
+        }
+
+        public static void exo_22()
+        {
+            Console.WriteLine("Veuillez taper un mot avec des # : ");
+            string? mot = Console.ReadLine();
+            string? res = mot.Replace('#', ' ');
+            Console.WriteLine(res);
+        }
+
+        public static void exo_23()
+        {
+            Console.WriteLine("Veuillez taper un mot pour tester s\'il est palindrome : ");
+            string? mot = Console.ReadLine();
+            bool isPalindrome = true;
+            int i = 0;
+            int j = mot.Length - 1;
+            while (i < mot.Length/2 && j > mot.Length/2) {
+                if(mot[i] != mot[j]) {
+                    isPalindrome = false;
+                    Console.WriteLine("Ce mot n\'est pas un palindrome.");
+                    break;
+                }
+                i++;
+                j--;
+            }
+            if (isPalindrome) {
+                Console.WriteLine("Ce mot est palindrome.");
+            }
+        }
     }
 }
